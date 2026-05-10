3d0a90bab44066e3de8513eb5d0ce11fb44ec4d4
silvia <silviaolivares@paucasesnovescifp.cat>
2026-05-10 18:32:26 +0200

    feat: implementar modelo de datos y persistencia con localStorage

---
+++ js/utils.js
@@ -0,0 +1,6 @@
+export function fechaAString(fecha) {
+    const dia = fecha.getDate().toString().padStart(2, '0');
+    const mes = (fecha.getMonth() + 1).toString().padStart(2, '0');
+    const año = fecha.getFullYear();
+    return `${año}-${mes}-${dia}`;
+}
